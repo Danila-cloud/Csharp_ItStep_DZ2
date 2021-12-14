30a679be8db7577ed9043b955d6851d10137d6ee
Danya Provilskyi <65830545+Danila-cloud@users.noreply.github.com>
2021-12-14 19:16:26 +0200

    Добавьте файлы проекта.

---
+++ Csharp2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp2
+{
+    public class rocket
+    {
+        private string PilotName;
+        private string RocketName;
+        private int AVGspeed;
+        private int speed;
+        private bool IsEngine;
+        private static bool IsStart = false;
+        private static int CountEngine = 4;
+
+        public rocket(string PilotName, string RocketName) { this.PilotName = PilotName;
+            this.RocketName = RocketName;
+        }
+
+        public rocket (string Rocketname) { this.RocketName = Rocketname; }
+
+        static rocket() { CountEngine = 6; }
+
+        public void Start()
+        {
+            Console.WriteLine("Rocket Start!!!!");
+        }
+
+        public void SpeedUp(int delta)
+        {
+            speed += delta;
+        }
+
+        public void EndTrip()
+        {
+            Console.WriteLine("Your trip by rocket is end!!!!");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
